106b218bc6a27a5fce35424e06de1d88f95c76cb
MiguSchweiz <miguschweiz@bluewin.ch>
2019-09-04 22:30:12 +0000

    check-in

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+apt-get update
+apt-get install kodi
+
+echo "copy Videos to /home/pi/Videos"
+echo "Press Any key to continue"
+read
+
+cd /home/pi/Videos
+rm pl.m3u
+ls >pl.m3u
+cd /home/pi/rpiVideoLoop
+
+mkdir -p /home/pi/.kodi/userdata/
+cp autoexec.py /home/pi/.kodi/userdata/
+
+cp videoloop.sh /home/pi/
+
+sudo cp videoloop.service /etc/systemd/system/videoloop.service
+sudo systemctl enable videoloop.service
+sudo systemctl start videoloop.service
+
